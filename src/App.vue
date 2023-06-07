254f6a9549bf494fcec2928185588aa748a67037
Reyval <Yudhareyval3@gmail.com>
2023-06-07 12:11:39 +0700

    test

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <Navbar></Navbar>
+  <Header></Header>
+  <About></About>
+  <Headmaster></Headmaster>
+  <Services></Services>
+  <Portfolio @project-clicked="_onProjectOpened"></Portfolio>
+  <Team></Team>
+  <Footer></Footer>
+  <PortfolioModal ref="portfolioModal"></PortfolioModal>
+</template>
+
+<script setup>
+import Navbar from "./components/Navbar.vue";
+import Header from "./sections/Header.vue";
+import About from "./sections/About.vue";
+import Headmaster from "./sections/Headmaster.vue";
+import Services from "./sections/Services.vue";
+import Team from "./sections/Team.vue";
+import Contact from "./sections/Contact.vue";
+import Footer from "./sections/Footer.vue";
+import Portfolio from "./sections/Portfolio.vue";
+import PortfolioModal from "./components/PortfolioModal.vue";
+import {onMounted, ref} from "vue";
+
+const portfolioModal = ref(null);
+
+const _onProjectOpened = (project) => {
+  portfolioModal.value.displayProject(project)
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/sections/Services.vue
@@ -0,0 +1,28 @@
+<template>
+  <PageSectionWrapper :id="servicesData.id" :title="servicesData.title" :subtitle="servicesData.subtitle">
+    <div class="row text-center">
+      <template v-for="item in servicesData.items">
+            <h5 class="my-3">{{item.title}}</h5>
+            <p class="text-muted" v-html="item.description"></p>
+      </template>
+    </div>
+  </PageSectionWrapper>
+</template>
+
+<script setup>
+import agencyData from '../data/agency.json'
+import PageSectionWrapper from "../components/PageSectionWrapper.vue";
+const servicesData = agencyData.services
+</script>
+
+<style scoped lang="scss">
+@import "../scss/_theming.scss";
+
+@include media-breakpoint-up(md) {
+  .item {
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+}
+
+</style>
